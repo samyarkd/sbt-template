c543956c8f83e31f65ec1d727b0b46ee2aa8b807
Samyar <75088294+samyarkd@users.noreply.github.com>
2024-01-27 09:47:31 +0000

    custom data

--- scripts/deployNftCollection.ts
+++ scripts/deployNftCollection.ts
@@ -6,7 +6,7 @@ import { SbtCollection } from "../wrappers/SbtCollection";
 
 export async function run(provider: NetworkProvider) {
     const OFFCHAIN_CONTENT_PREFIX = 0x01;
-    const string_first = "https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/"; // Change to the content URL you prepared
+    const string_first = "ipfs://QmXQzDC7AkmmqvdEDbyvwNZT1BcXv7G8bxEdiRZWovurnK/"; // Change to the content URL you prepared
     let newContent = beginCell().storeInt(OFFCHAIN_CONTENT_PREFIX, 8).storeStringRefTail(string_first).endCell();
 
     // ===== Parameters =====
@@ -15,13 +15,23 @@ export async function run(provider: NetworkProvider) {
 
     let collection = provider.open(await SbtCollection.fromInit(owner, newContent, {
         $$type: "RoyaltyParams",
-        numerator: 350n, // 350n = 35%
+        numerator: 0n, // 350n = 35%
         denominator: 1000n,
         destination: owner,
     }));
 
     // Do deploy
-    await collection.send(provider.sender(), {value: toNano("0.1")}, {$$type: 'RequestMint', index: 2n, owner_address: provider.sender().address!, authority_address: provider.sender().address!, content: beginCell().endCell()});
+    await collection.send(
+        provider.sender(),
+        {value: toNano("0.1")}, 
+        {
+            $$type: 'RequestMint', 
+            index: 0n, 
+            owner_address: provider.sender().address!,
+            authority_address: provider.sender().address!, 
+            content: beginCell().endCell()
+        }
+    )
 
     console.log(collection.address);
     // https://testnet.getgems.io/collection/EQBLzYrl72T2vUJxR4Ju7OgXU8E4KeUOMcu8RrD5HAhi-vkn
